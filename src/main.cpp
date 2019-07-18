9ec633b8ef44ffbce428ef023b3838815b4107a5
WBreno9 <williambreno9@gmail.com>
2019-07-18 16:29:34 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv) {
     if (argc != 2) std::exit(EXIT_FAILURE);
 
     HCompressor compressor;
-    compressor.compress_file_write(argv[1], "out.huff");
-    compressor.uncompress_file_write("out.huff", "out2.huff");
+    compressor.compressFileWrite(argv[1], "out.huff");
+    compressor.uncompressFileWrite("out.huff", "out2.huff");
 
     std::exit(EXIT_SUCCESS);
 }
