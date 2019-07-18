1eecb1ae09d0be6276fb648640e7e571cf08038d
WBreno9 <williambreno9@gmail.com>
2019-07-18 16:29:12 -0300

    Update hcompressor.cpp

--- src/hcompressor.cpp
+++ src/hcompressor.cpp
@@ -3,7 +3,7 @@
 HCompressor::HCompressor() {}
 HCompressor::~HCompressor() {}
 
-std::vector<uint8_t> HCompressor::compress_data(const std::vector<uint8_t>& data) {
+std::vector<uint8_t> HCompressor::compressData(const std::vector<uint8_t>& data) {
     std::array<uint32_t, CodeNum> freqs = {0};
     for (auto c : data) ++freqs[c];
 
@@ -22,7 +22,7 @@ std::vector<uint8_t> HCompressor::compress_data(const std::vector<uint8_t>& data
     return encoded_data;
 }
 
-std::vector<uint8_t> HCompressor::uncompress_data(const std::vector<uint8_t>& data) {
+std::vector<uint8_t> HCompressor::uncompressData(const std::vector<uint8_t>& data) {
     std::array<uint32_t, CodeNum> freqs = {0};
     std::vector<uint8_t> encoded(data.size() - CodeNum * sizeof(uint32_t));
 
@@ -36,15 +36,15 @@ std::vector<uint8_t> HCompressor::uncompress_data(const std::vector<uint8_t>& da
     return decoded;
 }
 
-std::vector<uint8_t> HCompressor::compress_file(const std::string& file_path) {
+std::vector<uint8_t> HCompressor::compressFile(const std::string& file_path) {
     std::ifstream input_file(file_path);
     std::vector<uint8_t> data((std::istreambuf_iterator<char>(input_file)), (std::istreambuf_iterator<char>()));
     input_file.close();
 
-    return compress_data(data);
+    return compressData(data);
 }
 
-std::vector<uint8_t> HCompressor::uncompress_file(const std::string& file_path) {
+std::vector<uint8_t> HCompressor::uncompressFile(const std::string& file_path) {
     std::ifstream input_file(file_path, std::ios::binary);
 
     input_file.seekg(0, input_file.end);
@@ -55,16 +55,16 @@ std::vector<uint8_t> HCompressor::uncompress_file(const std::string& file_path) 
     data.resize(size);
     input_file.read(reinterpret_cast<char*>(data.data()), data.size());
 
-    return uncompress_data(data);
+    return uncompressData(data);
 }
 
-void HCompressor::compress_file_write(const std::string& file_path, const std::string& write_file_path) {
+void HCompressor::compressFileWrite(const std::string& file_path, const std::string& write_file_path) {
     auto data = compress_file(file_path);
     std::ofstream fs(write_file_path);
     fs.write(reinterpret_cast<const char*>(data.data()), data.size());
 }
 
-void HCompressor::uncompress_file_write(const std::string& file_path, const std::string& write_file_path) {
+void HCompressor::uncompressFileWrite(const std::string& file_path, const std::string& write_file_path) {
     auto data = uncompress_file(file_path);
     std::ofstream fs(write_file_path);
     fs.write(reinterpret_cast<const char*>(data.data()), data.size());
